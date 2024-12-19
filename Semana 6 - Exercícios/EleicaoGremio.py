337abeda574508ee641d261b31692a990348c53f
Matheus Borges <40411599+NuncaMorto@users.noreply.github.com>
2024-12-19 18:19:34 +0000

    Comitando os códigos da aula pra poder sair e estudar pra gaal

--- Semana 6 - Exercícios/EleicaoGremio.py
+++ Semana 6 - Exercícios/EleicaoGremio.py
@@ -7,28 +7,27 @@ n1Quantidade = 0
 n2Quantidade = 0
 nInvalido = 0
 
-nEleitores = int(input("Forneça a quantidade de eleitores: "))
-if nEleitores < 3:
-    print("A quantidade de eleitores é inferior a 3")
+while True:
     nEleitores = int(input("Forneça a quantidade de eleitores: "))
-else:
-    print("## Votação Iniciada")
-    for i in range(nEleitores):
-        voto = int(input("Forneça o número do candidato que deseja votar: "))
-        if voto == n1:
-            n1Quantidade = n1Quantidade + 1
-        elif voto == n2:
-            n2Quantidade = n2Quantidade + 1
-        else:
-            nInvalido = nInvalido + 1
-    print("## Votação Encerrada")
+    if nEleitores >= 3:
+        break
+    print("A quantidade de eleitores é inferior a 3")
+
+print("## Votação Iniciada")
+for i in range(nEleitores):
+    voto = int(input("Forneça o número do candidato que deseja votar: "))
+    if voto == n1:
+        n1Quantidade += 1
+    elif voto == n2:
+        n2Quantidade += 1
+    else:
+        nInvalido += 1
+print("## Votação Encerrada")
 
 votosValidos = n1Quantidade + n2Quantidade
+totalVotos = votosValidos + nInvalido
 
-print(f"Votos válidos: {(votosValidos / 100):.2f}% ({votosValidos} votos)")
-print(f"Votos inválidos: {(nInvalido / 100):.2f}% ({nInvalido} votos)")
-print(f"Votos para {candidato1}: {(n1Quantidade / 100):.2f}%")
-print(f"Votos para {candidato2}: {(n2Quantidade / 100):.2f}%")
-    
-    
-    
+print(f"\nVotos válidos: {votosValidos / totalVotos * 100:.2f}% ({votosValidos} votos)")
+print(f"Votos inválidos: {nInvalido / totalVotos * 100:.2f}% ({nInvalido} votos)")
+print(f"Votos para {candidato1}: {n1Quantidade / votosValidos * 100:.2f}% ({n1Quantidade} votos)")
+print(f"Votos para {candidato2}: {n2Quantidade / votosValidos * 100:.2f}% ({n2Quantidade} votos)")
